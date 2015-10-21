344c7e05c5130d1682bd22adb2acb1aa4f4dd571
Joshua Caron <joshua@joshuacaron.ca>
2015-10-21 10:17:14 -0400

    Fixed css so that labels wrap properly and there is no overflow on very narrow screens.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,37 +8,65 @@ import {ToLatex} from './tolatex'
     <h1><span class="accent-color">tables</span>.joshuacaron.ca</h1>
     <p>Formats a table from a spreadsheet (e.g. a tab or comma separated value file) as a proper LaTeX table using booktabs.</p>
     <div class="layout horizontal wrap">
-      <label class="layout horizontal"><span class="flex">Number of decimal places to round numbers to (use -1 to not round at all): </span><input [(ng-model)]="precision"></label>
-      <label class="layout horizontal"><span class="flex">Text that separates columns (use '\\t' for tab): </span><input [(ng-model)]="separator"></label>
-      <label class="layout horizontal"><span class="flex">Use the first row as a header row: </span><input type="checkbox" [(ng-model)]="firstHeader"></label>
-      <label class="layout horizontal"><span class="flex">Escape all dollar signs: </span><input type="checkbox" [(ng-model)]="escapeDollarSigns"></label>
-      <label class="layout horizontal"><span class="flex">Add commas to large numbers: </span><input type="checkbox" [(ng-model)]="addCommas"></label>
+      <div class="column flex layout vertical">
+        <label class="layout horizontal wrap"><span class="flex">Number of decimal places to round numbers to (use -1 to not round at all):</span><input [(ng-model)]="precision" type="text"></label>
+        <label class="layout horizontal wrap"><span class="flex">Text that separates columns (use '\\t' for tab):</span><input [(ng-model)]="separator" type="text"></label>
+        <label class="layout horizontal wrap"><span class="flex">Use the first row as a header row:</span><input type="checkbox" [(ng-model)]="firstHeader"></label>
+      </div>
+      <div class="column flex layout vertical">
+        <label class="layout horizontal wrap"><span class="flex">Escape all dollar signs:</span><input type="checkbox" [(ng-model)]="escapeDollarSigns"></label>
+        <label class="layout horizontal wrap"><span class="flex">Add commas to large numbers:</span><input type="checkbox" [(ng-model)]="addCommas"></label>
+      </div>
     </div>
     <div class="layout horizontal flex wrap">
-      <div class="editor-size layout flex relative vertical"><ace-editor id="editor1" class="editor-size fit flex" [value]="code" (value-change)="output($event)" options='
+      <div class="editor-size layout flex relative vertical"><ace-editor id="editor1" class="fit flex" [value]="code" (value-change)="output($event)" options='
         {"theme":"monokai",
          "mode": "plain_text",
          "tabsize": 4,
          "softtabs": false,
          "focus": true}
       '></ace-editor></div>
-      <div class="layout flex relative vertical editor-size"><ace-editor id="editor2" class="editor-size fit flex" [value]="outputcode | tolatex:precision:separator:firstHeader:escapeDollarSigns:addCommas" options='{"theme":"monokai", "mode": "latex", "readOnly": true, "update": true}'></ace-editor></div>
+      <div class="editor-size layout flex relative vertical editor-size"><ace-editor id="editor2" class="fit flex" [value]="outputcode | tolatex:precision:separator:firstHeader:escapeDollarSigns:addCommas" options='{"theme":"monokai", "mode": "latex", "readOnly": true, "update": true}'></ace-editor></div>
     </div>
    `,
   styles: [`
     label {
-      margin: 7.5px 5px;
+      margin: 7.5px;
       max-width: 600px;
-      min-width: 600px;
+    }
+    @media (min-width: 441px){
+      .column {
+        min-width: 400px;
+      }
+      .editor-size {
+        min-width: 400px;
+      }
+      h1 {
+        font-size:2.5em;
+      }
+    }
+    @media (max-width: 440px) {
+      h1 {
+        font-size: 2em;
+      }
+      .editor-size {
+        min-width: 100%;
+      }
+      .column {
+        min-width: 100%;
+      }
     }
     label:last-child {
       margin-bottom: 15px;
     }
     input {
       width: 20px;
+      margin-left: 5px;
+    }
+    input[type="text"] {
+      height: 1.3em;
     }
     h1 {
-      font-size:2.5em;
       margin: 0.4em 0 0.2em;
     }
     p {
@@ -50,7 +78,7 @@ import {ToLatex} from './tolatex'
     }
     .editor-size {
       min-height: 300px;
-      min-width: 400px;
+      margin-top: 7.5px;
     }
   `],
   directives:[AceEditor,FORM_DIRECTIVES],
