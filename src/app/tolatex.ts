e3fc95ade1649178c116ae1edfea6fc3e214842f
Joshua Caron <joshua@joshuacaron.ca>
2015-10-21 22:13:35 -0400

    Fixed default function arguments for pipe and exported helper functions for sake of unit tests.

--- src/app/tolatex.ts
+++ src/app/tolatex.ts
@@ -3,35 +3,6 @@ import {Pipe, PipeTransform} from 'angular2/angular2'
 @Pipe({ name: "tolatex" })
 export class ToLatex implements PipeTransform {
   convert(text, fs, rs, precision, firstHeader, escapeDollarSigns, addCommas) {
-    function isNumber(x) {
-      var y = x.match(/[-]?[0-9.]+/g)
-      if (y !== null && y.length === 1 && y[0].length === x.length && x !== '.') {
-        return true
-      } else {
-        return false
-      }
-    }
-
-    function insertCommas(x) {
-      var w = x.split('.')
-      var y = w[0].split('')
-      var z = y.length
-      var i = z - 1
-      var output = ''
-      do {
-        console.log(z, i)
-        output = y[i] + output
-        if ((z - i) % 3 === 0 && i !== 0) {
-          output = ',' + output
-        }
-      } while (i--)
-      if (w.length > 1) {
-        return output + '.' + w[1]
-      } else {
-        return output
-      }
-    }
-
     var workingText = text
     // Fix line endings
     workingText = workingText.replace(/\r\n|\r/g, "\n")
@@ -96,26 +67,65 @@ export class ToLatex implements PipeTransform {
   }
 
   transform(value,args){
+    if (args === undefined) {
+      args = []
+    }
     // add commas
-    if (args[4] === undefined || args[4] === null) {
+    if (args[4] === undefined) {
       args[4] = false
     }
     // escape dollar signs
-    if (args[3] === undefined || args[3] === null) {
+    if (args[3] === undefined) {
       args[3] = true
     }
     // first row is header
-    if (args[2] === undefined || args[2] === null){
+    if (args[2] === undefined){
       args[2] = true
     }
     // column separator
-    if (args[1] === '' || args[1] === '\t' || args[1] === "\\t" || args[1] === undefined || args[2] === null) {
+    if (args[1] === undefined || args[1] === '' || args[1] === '\t' || args[1] === "\\t") {
       args[1] = '\t'
     }
     // precision
-    if (args[0] === undefined || args[0] === null) {
-      args[0] = 2
+    if (args[0] === undefined) {
+      args[0] = 3
     }
     return this.convert(value, args[1], '\n', args[0], args[2], args[3], args[4])
   }
+}
+
+export function isNumber(x) {
+    var y = x.match(/[-]?[0-9.]+/g)
+    if (y !== null && y.length === 1 && y[0].length === x.length && x !== '.') {
+        return true
+    } else {
+        return false
+    }
+}
+
+export function insertCommas(x) {
+    var leadingDigits = x.split('.')
+    var negative = false
+    var digits = leadingDigits[0].split('')
+    if (digits[0] === '-') {
+      digits.shift()
+      negative = true
+    }
+    var z = digits.length
+    var i = z - 1
+    var output = ''
+    do {
+        output = digits[i] + output
+        if ((z - i) % 3 === 0 && i !== 0) {
+            output = ',' + output
+        }
+    } while (i--)
+    if (negative) {
+      output = '-' + output
+    }
+    if (leadingDigits.length > 1) {
+        return output + '.' + leadingDigits[1]
+    } else {
+        return output
+    }
 }
